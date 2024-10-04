9aed305fd85e70f47c3e965ebdef914114cac624
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-05 02:05:41 +0300

    Task1 fully completed

--- Homeworks/Homework2/Task1/Task1.cpp
+++ Homeworks/Homework2/Task1/Task1.cpp
@@ -47,6 +47,39 @@ void dec2oct(int number) {
     cout << endl;
 }
 
+void dec2hex(int number){
+    cout << "Шестнайсетично: ";
+
+    if (number < 0) {
+        cout << "-";
+        number = -number;
+    }
+
+    if (number == 0) {
+        cout << "0" << endl;
+        return;
+    }
+
+
+    char hex[32];
+    int index = 0;
+
+    while (number > 0) {
+        int hexDigit = number & 0xF;
+        if (hexDigit < 10) {
+            hex[index++] = '0' +  hexDigit;
+        } else {
+            hex[index++] = 'A' + (hexDigit - 10);
+        }
+        number >>= 4;
+    }
+
+    for (int i = index -1; i >= 0; i--) {
+        cout << hex[i];
+    }
+    cout << endl;
+}
+
 int main(){
     int number;
     cout << "Въведете цяло десетично число: ";
@@ -54,7 +87,7 @@ int main(){
     
     dec2bin(number);
     dec2oct(number);
-    //dec2hex(number);
+    dec2hex(number);
 
     return 0;
 }
