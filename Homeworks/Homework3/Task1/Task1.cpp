32ae5cab304be40a5c94116588f61e4e3922e5ed
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-12 02:04:05 +0300

    Task1 Set attendance and Attendance info for present students implemented

--- Homeworks/Homework3/Task1/Task1.cpp
+++ Homeworks/Homework3/Task1/Task1.cpp
@@ -2,6 +2,12 @@
 #include <stdint.h>
 #include <limits>
 
+void pressEnterToContinue(){
+    std::cout << "Press Enter to continue...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+}
+
 int main(void)
 {
     uint64_t attendance = 0;
@@ -23,13 +29,50 @@ int main(void)
         }
         if (option == 1)
         {
-            // to do
+            std::cout << "Enter student number (1-64) to set attendance: ";
+            int student_number;
+            std::cin >> student_number;
+
+            if(std::cin.fail() || student_number < 1 || student_number > 64) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cerr << "Invalid student number! Please enter a number between 1 and 64.\n";
+                pressEnterToContinue();
+            } else {
+                attendance |= (uint64_t(1) << (student_number -1));
+                std::cout << "Attendance set for student " << student_number << ".\n";
+            }
         }
         else if (option == 2)
         {
         }
         else if (option == 3)
         {
+            std::cout << "1. Show present students" << std::endl;
+            std::cout << "2. Show absent students" << std::endl;
+            std::cout <<"Please select an option: ";
+            int sub_option;
+            std::cin >> sub_option;
+
+            if(std::cin.fail() || (sub_option != 1 && sub_option != 2)) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cerr << "Invalid option! Please enter 1 or 2.\n";
+            }else if (sub_option == 1) {
+                if(attendance){
+                    std::cout << "Present students: ";
+                    for (int i = 1; i <= 64; ++i){
+                        if (attendance & (uint64_t(1) << (i-1))){
+                            std::cout << i << " ";
+                        }
+                    }
+                    std::cout << std::endl;
+                    pressEnterToContinue();
+                } else {
+                    std::cout << "No students are present. \n";
+                    pressEnterToContinue();
+                }
+            }
         }
         else if (option == 4)
         {
