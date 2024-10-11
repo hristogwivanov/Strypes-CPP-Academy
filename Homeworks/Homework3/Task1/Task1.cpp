f6290bde9257b5315e53290d61ae0251e5dd286c
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-11 12:22:48 +0000

    Task 1 skeleton created

---
+++ Homeworks/Homework3/Task1/Task1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdint.h>
+
+int main(void)
+{
+    uint64_t attendance = 0;
+    int option;
+    while (1)
+    {
+        std::cout << "1. Set attendamce" << std::endl;
+        std::cout << "2. Clear attendance" << std::endl;
+        std::cout << "3. Attendance info " << std::endl;
+        std::cout << "4. Change attendance" << std::endl;
+        std::cout << "5. Exit" << std::endl;
+        std::cin >> option;
+        if (option == 1){
+            // to do
+        }
+        else if (option == 2) 
+        {
+
+        }
+        else if (option == 3)
+        {
+
+        }
+        else if (option == 4) {
+
+        }
+        else if (option == 5) {
+            return 0;
+        } else {
+
+        }
+    }
+}
