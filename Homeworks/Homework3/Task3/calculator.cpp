96e80f61456215a7e060b28e31f24db1d3d57172
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-14 11:42:37 +0000

    Task3 first argument validation implemented

--- Homeworks/Homework3/Task3/calculator.cpp
+++ Homeworks/Homework3/Task3/calculator.cpp
@@ -9,4 +9,14 @@ int main(int argc, char* argv[]){
         cerr << "Грешка - броят на аргументите тярбва да е точно 3!\n";
         return 1;
     }
+
+    char* inputNum1;
+    char* inputNum2;
+
+    double num1 = strtod(argv[1], &inputNum1);
+    if (*inputNum1 != '\0'){
+        cerr << "Първият аргумент трябва да бъде цисло" << endl;
+        return 1;
+    }    
+
 }
