9fe3808ac8a7fc2a0108981e5429866a39f9c2f8
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-14 11:11:47 +0000

    Task3 args number check implemented

---
+++ Homeworks/Homework3/Task3/calculator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdlib>
+
+using namespace std;
+
+int main(int argc, char* argv[]){ 
+    if (argc != 4) {
+        cerr << "Грешка - броят на аргументите тярбва да е точно 3!\n";
+        return 1;
+    }
+}
