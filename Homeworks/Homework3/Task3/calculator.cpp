e5dd56472ccbc659c664c72d8cc5a0d00b360769
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-14 11:47:22 +0000

    Second number validation implemented

--- Homeworks/Homework3/Task3/calculator.cpp
+++ Homeworks/Homework3/Task3/calculator.cpp
@@ -17,6 +17,12 @@ int main(int argc, char* argv[]){
     if (*inputNum1 != '\0'){
         cerr << "Първият аргумент трябва да бъде цисло" << endl;
         return 1;
-    }    
+    }
+
+    double num2 = strtod(argv[3], &inputNum2);
+    if (*inputNum2 != '\0'){
+        cerr << "Вторият аргумент трябва да бъде число" << endl;
+        return 1;
+    }
 
 }
