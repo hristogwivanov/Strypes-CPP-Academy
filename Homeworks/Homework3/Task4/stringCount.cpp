8987fa54e485618256dd845a09a1cf65a1bb097a
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-14 13:19:30 +0000

    Task 4 input validation implemented!

---
+++ Homeworks/Homework3/Task4/stringCount.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+    if (argc != 2)
+    {
+        cerr << "Please enter only one quoted expression as an argument!" << endl;
+    }
+
+}
