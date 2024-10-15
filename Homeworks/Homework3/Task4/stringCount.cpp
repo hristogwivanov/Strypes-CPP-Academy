704a57dbc5461477623cf89b070efbfba3d86435
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-15 08:33:36 +0000

    Task4 fully completed!

--- Homeworks/Homework3/Task4/stringCount.cpp
+++ Homeworks/Homework3/Task4/stringCount.cpp
@@ -4,9 +4,25 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+        if (argc == 1)
+    {
+        cerr << "Please enter a quoted expression or a single string/number as an argument!" << endl;
+        return 1;
+    }
     if (argc != 2)
     {
-        cerr << "Please enter only one quoted expression as an argument!" << endl;
+        cerr << "Please enter only one string/number or a quoted expression as an argument!" << endl;
+        return 1;
     }
 
+    string searchString = argv[1];
+    string inputString; 
+    int count = 1;
+
+    while (getline(cin, inputString)){
+        if (inputString == searchString) count++;
+    }
+
+    cout << "Count = " << count << endl;
+    return 0;
 }
