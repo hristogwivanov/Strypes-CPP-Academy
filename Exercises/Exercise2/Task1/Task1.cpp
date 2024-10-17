211e376c4f890f0f6a863b4d0bef3420cdfb1e7e
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-17 12:05:21 +0000

    Task1 Completed

--- Exercises/Exercise2/Task1/Task1.cpp
+++ Exercises/Exercise2/Task1/Task1.cpp
@@ -1,32 +1,162 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <bitset>
+#include <stdexcept>
 
 using namespace std;
 
-int main() {
+void convertBinary(const string &numStr)
+{
+    int num;
+    try
+    {
+        num = stoi(numStr, nullptr, 2); 
+    }
+    catch (const invalid_argument &)
+    {
+        cout << "Невалиден двоичен номер.\n";
+        return;
+    }
+    catch (const out_of_range &)
+    {
+        cout << "Номерът е извън допустимия диапазон.\n";
+        return;
+    }
+
+    cout << "Десетично: " << num << endl;
+    cout << "Осмично: " << std::oct << num << std::dec << endl;
+    cout << "Шестнадесетично: " << std::hex << num << std::dec << endl;
+}
+
+void convertOctal(const string &numStr)
+{
+    int num;
+    try
+    {
+        num = stoi(numStr, nullptr, 8); 
+    }
+    catch (const invalid_argument &)
+    {
+        cout << "Невалиден осмичен номер.\n";
+        return;
+    }
+    catch (const out_of_range &)
+    {
+        cout << "Номерът е извън допустимия диапазон.\n";
+        return;
+    }
+
+    cout << "Десетично: " << num << endl;
+    cout << "Двоично: " << std::bitset<32>(num) << endl;
+    cout << "Шестнадесетично: " << std::hex << num << std::dec << endl;
+}
+
+void convertDecimal(const string &numStr)
+{
+    int num;
+    try
+    {
+        num = stoi(numStr);
+    }
+    catch (const invalid_argument &)
+    {
+        cout << "Невалиден десетичен номер.\n";
+        return;
+    }
+    catch (const out_of_range &)
+    {
+        cout << "Номерът е извън допустимия диапазон.\n";
+        return;
+    }
+
+    cout << "Двоично: " << std::bitset<32>(num) << endl;
+    cout << "Осмично: " << std::oct << num << std::dec << endl;
+    cout << "Шестнадесетично: " << std::hex << num << std::dec << endl;
+}
+
+void convertHex(const string &numStr)
+{
+    int num;
+    try
+    {
+        num = stoi(numStr, nullptr, 16); // Parse numStr as base 16
+    }
+    catch (const invalid_argument &)
+    {
+        cout << "Невалиден шестнадесетичен номер.\n";
+        return;
+    }
+    catch (const out_of_range &)
+    {
+        cout << "Номерът е извън допустимия диапазон.\n";
+        return;
+    }
+
+    cout << "Десетично: " << num << endl;
+    cout << "Двоично: " << std::bitset<32>(num) << endl;
+    cout << "Осмично: " << std::oct << num << std::dec << endl;
+}
+
+int main()
+{
     int choice;
     bool valid = false;
 
-    while(!valid) {
-        cout << "Моля въведете в каква бройна система е входното число (2, 4, 8 или 16): " << endl;
+    while (!valid)
+    {
+        cout << "Моля въведете в каква бройна система е входното число (2, 8, 10 или 16): " << endl;
         string input;
         getline(cin, input);
         stringstream ss(input);
 
-        if(ss >> choice) {
-            if(ss.eof()){
-                if(choice == 2 || choice == 8 || choice == 16) {
+        if (ss >> choice)
+        {
+            if (ss.eof())
+            {
+                if (choice == 2 || choice == 8 || choice == 10 || choice == 16)
+                {
                     valid = true;
-                } else {
+                }
+                else
+                {
                     cout << "Невалиден номер. Моля въведете 2, 8, 10 или 16. \n";
                 }
-            } else {
+            }
+            else
+            {
                 cout << "Невалиден вход. Моля използвайте числови стойности!\n";
             }
-        } else {
+        }
+        else
+        {
             cout << "Невалиден вход. Моля използвайте числови стойности!\n";
         }
     }
+
+    string numStr;
+
+    cout << "Моля въведете номер който да бъде конвертиран:";
+    getline(cin, numStr);
+
+    switch (choice)
+    {
+    case 2:
+        convertBinary(numStr);
+        break;
+    case 8:
+        convertOctal(numStr);
+        break;
+    case 10:
+        convertDecimal(numStr);
+        break;
+    case 16:
+        convertHex(numStr);
+        break;
+    default:
+        cout << "Невалиден избор.\n";
+        break;
+    }
+
     return 0;
 }
