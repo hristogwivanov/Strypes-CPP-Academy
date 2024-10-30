d591a75d061caa9a7d0f7c76fe1ddfb568a8d259
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-30 09:39:54 +0000

    Task9 completed!

--- Exercises/Exercise1/Task09/Task09.cpp
+++ Exercises/Exercise1/Task09/Task09.cpp
@@ -1,5 +1,16 @@
 #include <iostream> 
+#include <cmath> 
 using namespace std; 
 int main () {
+    float a, b, area, perimeter; 
+    cout << "Пресмятане на площта и периметъра на правоъгълник" << endl; 
+    cout << "Въведете първата страна на правоъгълника: ";
+    cin >> a; 
+    cout << "Въведете втората страна на правоъгълника: ";
+    cin >> b; 
+    area = a*b/2; 
+    perimeter = (a + b) + sqrt(a*a + b*b);
+    cout << "Площта на правоъгълника е: " << area << endl;
+    cout << "Периметъра на правоъгълника е: " << perimeter << endl; 
     return 0;
 }
