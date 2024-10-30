3e016a5a0e8f12242c7aa38f1d719f33215aa171
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-30 09:55:54 +0000

    Task11 completed!

---
+++ Exercises/Exercise1/Task11/Task11.cpp
@@ -0,0 +1,10 @@
+#include <iostream> 
+using namespace std;
+int main () {
+    float a; 
+    cout << "Изчисляване на обем на куб" << endl;
+    cout << "Моля въведете дължина на ръба на куба: ";
+    cin >> a;
+    cout << "Обема на куба е: " << a * a * a << endl; 
+    return 0;
+}
