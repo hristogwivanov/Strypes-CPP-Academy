9942a610bcb0886d1de37949f5cfe482689e897e
Hristo Ivanov <gw.hivanov@gmail.com>
2024-10-30 14:20:01 +0000

    Task12 completed!

---
+++ Exercises/Exercise1/Task12/Task12.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std; 
+int main() { 
+    int a, b; 
+    cout << "Разлика между 2 числа." << endl;
+    cout << "Въведете първото число: ";
+    cin >> a;
+    cout << "Въведете второто число: ";
+    cin >> b; 
+    cout << "Разликата между числата е: " << ((a > b) ? (a-b) : (b-a)) << endl;
+
+}
