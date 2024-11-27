50da1aca7516f2a403f1c6490443ab43e6bf618f
Hristo Ivanov <gw.hivanov@gmail.com>
2024-11-27 13:57:34 +0000

    Task16 completed.

---
+++ Exercises/Exercise1/Task16/Task16.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n, buff, sum = 0;
+    cout << "Моля, въведете брой на въведените числа: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cout << "Моля, въведете число номер " << i << ":"; 
+        cin >> buff; 
+        sum += buff;
+    }
+
+    cout << "Средната стойност на всички " << n << " числа е " << static_cast<float>(sum)/n << endl;
+
+    return 0;
+}
