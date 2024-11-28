6db435fcb6b0fec143598318890a2da9947278e4
Hristo Ivanov <gw.hivanov@gmail.com>
2024-11-28 08:08:34 +0000

    Task17 completed

--- Exercises/Exercise1/Task17/Task17.cpp
+++ Exercises/Exercise1/Task17/Task17.cpp
@@ -1,1 +1,15 @@
-#
+#include<iostream>
+using namespace std; 
+int main(){
+    float a, b;
+    cout << "Моля въведете първото число: ";
+    cin >> a; 
+    cout << "Моля въведете второто число: ";
+    cin >> b;
+    cout << "Разликата между двете числа е: ";
+    if(a>b){
+        cout << (a-b) << endl;
+    }else cout << (b-a) << endl;
+    
+    return 0; 
+}
