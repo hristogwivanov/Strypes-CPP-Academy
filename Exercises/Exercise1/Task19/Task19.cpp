4fd70d835efcef8639507d4194ac5b03376afdb1
Hristo Ivanov <gw.hivanov@gmail.com>
2024-11-28 09:33:24 +0000

    Task20 completed

---
+++ Exercises/Exercise1/Task19/Task19.cpp
@@ -0,0 +1,10 @@
+#include<iostream> 
+using namespace std;
+int main() {
+    cout<< "Моля въведете 3 цели числа, разделени с интервал: ";
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << "Резултата от умножението е: " << a*b*c << endl;
+
+    return 0; 
+}
