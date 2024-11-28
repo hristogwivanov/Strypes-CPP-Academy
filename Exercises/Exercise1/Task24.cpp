6668729f06f8ea727589487e65e6d27c3d2aa757
Hristo Ivanov <gw.hivanov@gmail.com>
2024-11-28 13:03:26 +0000

    Task24 completed

---
+++ Exercises/Exercise1/Task24.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    cout << "Моля въведете число: ";
+    float a; 
+    cin >> a;
+    cout << "Квадратен корен е " << sqrt(a) << endl; 
+
+    return 0;
+}
