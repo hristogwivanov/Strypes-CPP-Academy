bca1a2ea0dbbcfdbf0c0c8e56489dc06d21debeb
Hristo Ivanov <gw.hivanov@gmail.com>
2024-11-28 12:39:32 +0000

    Task23 completed

---
+++ Exercises/Exercise1/Task23/Task23.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout << "Въведете число: "; 
+    int a; 
+    cin >> a;
+    cout << "Коренът на числото е " << a*a << endl; 
+    
+    return 0;
+}
